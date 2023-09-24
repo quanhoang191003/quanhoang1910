6f9b5643542dfbebfcf2f3170bf92704a25611b0
quanhoang191003 <quanhoang191003@gmail.com>
2023-09-24 14:59:28 +0700

    Last Update

--- Lab3/Ex8.3.php
+++ Lab3/Ex8.3.php
@@ -10,7 +10,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
-
+// Insert datas
 $sql = "INSERT INTO STUDENTS(ID, NAME, CLASS, MARK, SEX)
 VALUES (1,'John Deo','Four',75,'female');";
 $sql .= "INSERT INTO STUDENTS(ID, NAME, CLASS, MARK, SEX)
@@ -32,43 +32,50 @@ VALUES (9,'Tes Qry','Six',78,'male');";
 $sql .= "INSERT INTO STUDENTS(ID, NAME, CLASS, MARK, SEX)
 VALUES (10,'Big John','Four',55,'female')";
 
-
-$sql = "UPDATE STUDENTS SET CLASS = 'Two' WHERE MARK < 60";
-
-// SQL query to select best students (marks > 75)
-$best_students_sql = "SELECT * FROM STUDENTS WHERE MARK > 75";
-$best_students_result = $conn->query($best_students_sql);
-
-// SQL query to select good students (marks >= 60 and <= 75)
-$good_students_sql = "SELECT * FROM STUDENTS WHERE MARK >= 60 AND MARK <= 75";
-$good_students_result = $conn->query($good_students_sql);
-
-// SQL query to select average students (marks < 60)
-$average_students_sql = "SELECT * FROM STUDENTS WHERE MARK < 60";
-$average_students_result = $conn->query($average_students_sql);
-
-// Function to display a table for a result set
-function displayTable($result, $title) {
-    echo "<h2>$title</h2>";
-    echo "<table border='1'>";
-    echo "<tr><th>ID</th><th>Name</th><th>Mark</th><th>Class</th></tr>";
-    
-    while ($row = $result->fetch_assoc()) {
-        echo "<tr><td>".$row["ID"]."</td><td>".$row["NAME"]."</td><td>".$row["MARK"]."</td><td>".$row["CLASS"]."</td></tr>";
-    }
-    
-    echo "</table>";
-}
-
-// Display tables for best, good, and average students
-displayTable($best_students_result, "Best Students (Marks > 75)");
-displayTable($good_students_result, "Good Students (Marks > 60 and <= 75)");
-displayTable($average_students_result, "Average Students (Marks < 60)");
 if ($conn->multi_query($sql) === TRUE) {
-    echo "New records created successfully";
+    echo "Data is inserted successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
+
+
+// $sql = "UPDATE STUDENTS SET CLASS = 'Two' WHERE MARK < 60";
+
+// if ($conn->multi_query($sql) === TRUE) {
+//     echo "New records created successfully";
+// } else {
+//     echo "Error: " . $sql . "<br>" . $conn->error;
+// }
+
+// // SQL query to select best students (marks > 75)
+// $best_students_sql = "SELECT * FROM STUDENTS WHERE MARK > 75";
+// $best_students_result = $conn->query($best_students_sql);
+
+// // SQL query to select good students (marks >= 60 and <= 75)
+// $good_students_sql = "SELECT * FROM STUDENTS WHERE MARK >= 60 AND MARK <= 75";
+// $good_students_result = $conn->query($good_students_sql);
+
+// // SQL query to select average students (marks < 60)
+// $average_students_sql = "SELECT * FROM STUDENTS WHERE MARK < 60";
+// $average_students_result = $conn->query($average_students_sql);
+
+// // Function to display a table for a result set
+// function displayTable($result, $title) {
+//     echo "<h2>$title</h2>";
+//     echo "<table border='1'>";
+//     echo "<tr><th>ID</th><th>Name</th><th>Mark</th><th>Class</th></tr>";
+    
+//     while ($row = $result->fetch_assoc()) {
+//         echo "<tr><td>".$row["ID"]."</td><td>".$row["NAME"]."</td><td>".$row["MARK"]."</td><td>".$row["CLASS"]."</td></tr>";
+//     }
+    
+//     echo "</table>";
+// }
+
+// // Display tables for best, good, and average students
+// displayTable($best_students_result, "Best Students (Marks > 75)");
+// displayTable($good_students_result, "Good Students (Marks > 60 and <= 75)");
+// displayTable($average_students_result, "Average Students (Marks < 60)");
 
 $conn->close();
 ?>
